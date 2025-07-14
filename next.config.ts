447caf080d7ec25e2708526d50837f32f4a53939
unknown <upmerce.com@gmail.com>
2025-07-14 23:44:57 +0100

    Images remote

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
  
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'firebasestorage.googleapis.com',
+                port: '',
+                pathname: '/v0/**',
+            },
+        ],
+    },
+};
  
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
